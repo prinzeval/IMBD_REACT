07d05d415e40aaeb6a14bde092e5258bcdb1ee60
prinzeval <valentineallpowers@gmail.com>
2024-11-17 10:25:29 +0200

    learning react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import {useState} from 'react'
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 
 
 
 
 const App = () => {
+  const  [counter,setCounter] = useState(0);
   return (
-    <div className="APPchance">
-      <><h1>
-        hello world
-      </h1>
-      </>
+    <div className="APP">
+      <button onClick={()=>setCounter((prevCount)=>prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((newCount)=>newCount + 1)}>+</button>
+      
       
     </div>
   );
