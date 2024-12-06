ec96b4e6afbd5f1a6fea16189e525659a8a47e9d
prinzeval <valentineallpowers@gmail.com>
2024-12-06 15:26:48 +0200

    welcome Description

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = () => {
       <h1>Welcome To Mytvv Free Movies, Home To Paradise!</h1>
       <h2>Movie Are Organised Using Imdb Details, Top trending movies and Shows  </h2>
       <h3>Enjoy over 3000+ international series, and non-stop movies. <br />Get it, click it, and enjoy it for a lifetime.</h3>
-      <p>Search For Your Favorite Movies And TV Shows. Deskstop View Is Availiable, Mobile Is Coming Soon </p>
+      <p>Search For Your Favorite Movies And TV Shows. Deskstop View Is Availiable With Extra Features, Mobile View Tailored With Nice Taste ☺️ </p>
     </div>
   );
 };
