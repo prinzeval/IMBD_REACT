50ac05ab9e750596bbb85eadda82dda75a917ec9
prinzeval <valentineallpowers@gmail.com>
2024-12-12 00:33:23 +0200

    google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-39VDMNK3NP");
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: " window.location.pathname", 
+  title: " window.document.title" 
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
