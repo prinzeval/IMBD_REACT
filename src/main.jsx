60f983ac5a40a05572de99aa09d7260adec0cb9a
prinzeval <valentineallpowers@gmail.com>
2024-12-21 01:09:42 +0200

    delection

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-39VDMNK3NP");
-ReactGA.send({ 
-  hitType: "pageview", 
-  page: " window.location.pathname", 
-  title: " window.document.title" 
-});
+// ReactGA.initialize("G-39VDMNK3NP");
+// ReactGA.send({ 
+//   hitType: "pageview", 
+//   page: " window.location.pathname", 
+//   title: " window.document.title" 
+// });
 
 
 createRoot(document.getElementById('root')).render(
