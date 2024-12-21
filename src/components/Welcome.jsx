198ef554a046f7a67eaed19059bc21ecc09babad
prinzeval <valentineallpowers@gmail.com>
2024-12-21 23:53:06 +0200

    ADS REMOVAL

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from "react";
-// import "./Welcome.css"; // Uncomment and make sure to create this CSS file if needed
+import React from "react";
 
 const Welcome = () => {
-  useEffect(() => {
-    // Create a script element for the popunder ad
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "//pl25321094.profitablecpmrate.com/5c/85/f2/5c85f28f1156b8d6b1c2153d6d8bb74e.js";
-    script.async = true;
 
-    
 
-    // Append the script to the document body
-    document.body.appendChild(script);
-
-    // Clean up: Remove the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
   return (
     <div className="welcome">
